b9553bcdd7785d178e58c625c6d1e03b6c0f6efc
Adam Davidson <agdavidson91@gmail.com>
2025-01-18 09:09:13 -0500

    fix weight

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -1,18 +1,18 @@
-your_age = int(input("What is your age? "))
-age = 22
-string_age = str(age)
+# your_age = int(input("What is your age? "))
+# age = 22
+# string_age = str(age)
 
-if your_age >= 21:
-    print(f'You can drink since you are {your_age - 21} years above the drinking cutoff! ')
-else:
-    print(f'You can NOT drink!')
+# if your_age >= 21:
+#     print(f'You can drink since you are {your_age - 21} years above the drinking cutoff! ')
+# else:
+#     print(f'You can NOT drink!')
     
     
-quantity = int(input("How many pizzas do you want? "))
-price = 6.99
-total = quantity * price
+# quantity = int(input("How many pizzas do you want? "))
+# price = 6.99
+# total = quantity * price
 
-print(f'You ordered {quantity} pizzas and your total is ${total}! ')
+# print(f'You ordered {quantity} pizzas and your total is ${total}! ')
 
 my_weight = float(input("What is your weight? "))
 unit = input("Kilograms or Lbs? (K or L): ").upper()
@@ -20,10 +20,11 @@ unit = input("Kilograms or Lbs? (K or L): ").upper()
 if unit == "K":
     my_weight = my_weight * 2.205
     unit = "Lbs"
+    print(f'Your weight is: {round(my_weight, 2)} {unit}!')
 elif unit == "L":
     my_weight = my_weight / 2.205
     unit = "Kgs"
+    print(f'Your weight is: {round(my_weight, 2)} {unit}!')
 else:
     print(f'{unit} was not valid')
 
-print(f'Your weight is: {round(my_weight, 2)} {unit}!')

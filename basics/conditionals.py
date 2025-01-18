43cf7a1d358fd57824e30e32817cacf1e3770e36
Adam Davidson <agdavidson91@gmail.com>
2025-01-17 23:39:18 -0500

    conditions

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -6,3 +6,10 @@ if your_age >= 21:
     print(f'You can drink since you are {your_age - 21} years above the drinking cutoff! ')
 else:
     print(f'You can NOT drink!')
+    
+    
+quantity = int(input("How many pizzas do you want? "))
+price = 6.99
+total = quantity * price
+
+print(f'You ordered {quantity} pizzas and your total is ${total}! ')

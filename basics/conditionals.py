1e82508840c6387e43fa5941188d31b27c325e67
Adam Davidson <agdavidson91@gmail.com>
2025-01-18 09:05:50 -0500

    weight

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -13,3 +13,17 @@ price = 6.99
 total = quantity * price
 
 print(f'You ordered {quantity} pizzas and your total is ${total}! ')
+
+my_weight = float(input("What is your weight? "))
+unit = input("Kilograms or Lbs? (K or L): ").upper()
+
+if unit == "K":
+    my_weight = my_weight * 2.205
+    unit = "Lbs"
+elif unit == "L":
+    my_weight = my_weight / 2.205
+    unit = "Kgs"
+else:
+    print(f'{unit} was not valid')
+
+print(f'Your weight is: {round(my_weight, 2)} {unit}!')

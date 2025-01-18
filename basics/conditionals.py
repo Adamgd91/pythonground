a4b187b8e1a4d1a1135f31a6cf694b94c459c80b
Adam Davidson <agdavidson91@gmail.com>
2025-01-17 23:29:54 -0500

    fun fun

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -1,5 +1,6 @@
 your_age = int(input("What is your age? "))
-age= 22
+age = 22
+string_age = str(age)
 
 if your_age >= 21:
     print(f'You can drink since you are {your_age - 21} years above the drinking cutoff! ')

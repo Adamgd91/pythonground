5cc9a0732ef9e7cdee7535ab00d557c8633a02e6
Adam Davidson <agdavidson91@gmail.com>
2025-01-19 13:45:34 -0500

    testing

--- basics/dicts.py
+++ basics/dicts.py
@@ -1,6 +1,10 @@
 books = {
     "LOTR" : 432,
     "Potter" : 333,
-    "Fourth Wing" 392,
-    "Authors", ["Adam", "Sarah", "Julia"]
+    "Fourth Wing": 392,
+    "Authors": ["Adam", "Sarah", "Julia"]
 }
+
+print(books.values())
+print(books.items())
+print(books.keys())

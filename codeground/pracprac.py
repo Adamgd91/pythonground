769def04313e9a80bce76c501fe4114c2bac5b76
Adam Davidson <agdavidson91@gmail.com>
2025-01-20 16:04:40 -0500

    have fun

---
+++ codeground/pracprac.py
@@ -0,0 +1,11 @@
+job_title = "Data Scientist"
+upper_job_title = job_title.upper()
+print(type(job_title))
+print(upper_job_title)
+print(len(job_title))
+
+salary = 85000.50
+print(type(salary))
+
+salary_rate = int(120000.75)
+print(type(salary_rate))

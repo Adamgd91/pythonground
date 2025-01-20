e17674e3cf1d7c03ce5165fd85822813dcc0ce14
Adam Davidson <agdavidson91@gmail.com>
2025-01-20 16:12:36 -0500

    joy

--- codeground/pracprac.py
+++ codeground/pracprac.py
@@ -4,8 +4,11 @@ print(type(job_title))
 print(upper_job_title)
 print(len(job_title))
 
-salary = 85000.50
+salary = 185000.50
 print(type(salary))
 
 salary_rate = int(120000.75)
 print(type(salary_rate))
+
+
+print(f'I will become a {upper_job_title} and enjoy the tough times and the GOOD times!')

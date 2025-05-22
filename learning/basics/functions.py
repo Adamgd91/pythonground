cf5a8426816d9466b23c56465f8467fa6a7e124a
Adam Davidson <agdavidson91@gmail.com>
2025-05-22 07:52:55 -0400

    func

--- learning/basics/functions.py
+++ learning/basics/functions.py
@@ -7,7 +7,7 @@ def students(is_online):
     else:
         print("Offline")
     
-students(False)
+students(is_student)
 
 name = "adam"
 age = 34
@@ -20,3 +20,11 @@ def testing():
     
 testing()
 print(f"My name is {name} and I am {age} years old!!!")
+
+ages = [23, 34, 54, 31, 21, 77, 87, 21]
+
+def oldest():
+    the_oldest = max(ages)
+    print(the_oldest)
+    
+oldest()

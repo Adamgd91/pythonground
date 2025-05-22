0a481f77200b399b797a2e213ddbbd45c9e22d59
Adam Davidson <agdavidson91@gmail.com>
2025-05-22 10:57:00 -0400

    duncs

--- learning/basics/functions.py
+++ learning/basics/functions.py
@@ -8,19 +8,15 @@ def students(is_online):
         print("Offline")
     
 students(is_student)
-
-name = "adam"
-age = 34
-print(name)
-
+#################################################
 def testing():
     x = 4
     if x > 3:
         print("hello")
     
 testing()
-print(f"My name is {name} and I am {age} years old!!!")
 
+#################################################
 ages = [23, 34, 54, 31, 21, 77, 87, 21]
 
 def oldest():

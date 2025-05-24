fd60f7a75bed61448555cc0de96fad86c7bfbf5e
Adam Davidson <agdavidson91@gmail.com>
2025-05-24 07:42:15 -0400

    lists

--- learning/basics/lists.py
+++ learning/basics/lists.py
@@ -3,20 +3,17 @@ kit = 18.0
 liv = 20.0
 bed = 10.75
 bath = 9.50
-
 # Create list areas
 areas = [hall, kit, liv, bed, bath]
 # Print areas
 print(areas)
 print(areas[2])
-
 # House information as list of lists
 house = [["hallway", hall],
          ["kitchen", kit],
          ["living room", liv],
         ["bedroom", bed],
         ["bathroom", bath]]
-
 # Print out house
 print(house)
 bathroom_length = house[-1][1]
@@ -30,3 +27,11 @@ areas_copy = list(areas)
 areas_copy[0] = 5.0
 # Print areas
 print(areas)
+##############################################
+scores = [99, 15, 65, 43, 22, 28, 100, 98, 64, 66, 54, 76, 87, 88, 34, 56, 65, 46, 76, 65]
+reversed_scores = sorted(scores, reverse = True)
+print(reversed_scores)
+max_score = max(scores)
+print(max_score)
+avg_score = sum(scores) / len(scores)
+print(avg_score)

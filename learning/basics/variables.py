270a187735b2add2680cac6580556b6dfc7d56fe
Adam Davidson <agdavidson91@gmail.com>
2025-05-25 08:23:48 -0400

    asdd

--- learning/basics/variables.py
+++ learning/basics/variables.py
@@ -15,3 +15,12 @@ up_name = name.upper()
 count_name = name.count("a")
 print(count_name)
 print(up_name)
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))

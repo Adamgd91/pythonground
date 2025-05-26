894ecd7369eefb3135ba4adbf6cebc70c3f92e36
Adam Davidson <agdavidson91@gmail.com>
2025-05-26 09:11:25 -0400

    looping

--- learning/basics/loops.py
+++ learning/basics/loops.py
@@ -1,4 +1,14 @@
 my_family = ["Adam", "Sarah", "Harper", "Preston"]
-
+men = []
+women = []
 for i in my_family:
     print(i)
+    
+for m in my_family:
+    if m == "Adam" or m == "Preston":
+        men.append(m)
+    else:
+        women.append(m)
+
+print(men)
+print(women)

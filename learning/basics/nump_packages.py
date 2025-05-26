15f1813b5bd692229cc0be1dbdfd817830c09056
Adam Davidson <agdavidson91@gmail.com>
2025-05-26 07:54:27 -0400

    ray

--- learning/basics/nump_packages.py
+++ learning/basics/nump_packages.py
@@ -1,4 +1,6 @@
 import numpy as np
 
-testing = np.array([1,3,5,7,9])
-print(testing)
+height = np.array([1.65,3,5,7,9])
+weight = np.array([33, 43, 32, 34, 67])
+bmi = weight / height ** 2
+print(bmi)

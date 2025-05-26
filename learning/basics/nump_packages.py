f94b440e30d6797fc64d4cb38b6c3b3f6ff920aa
Adam Davidson <agdavidson91@gmail.com>
2025-05-26 07:58:34 -0400

    asd

--- learning/basics/nump_packages.py
+++ learning/basics/nump_packages.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-height = np.array([1.65,3,5,7,9])
-weight = np.array([33, 43, 32, 34, 67])
+height = np.array([1.65, 1.6, 2.1, 1.4, 3.2])
+weight = np.array([133, 163, 132, 84, 97])
 bmi = weight / height ** 2
 print(bmi)
+
+over_weight = bmi[bmi > 22]
+print(over_weight)

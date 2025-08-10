23b7597366dc67c1beece52617dc06b6d8ea9744
Adam Davidson <agdavidson91@gmail.com>
2025-08-10 15:15:32 -0400

    learning

--- learning/basics/variables.py
+++ learning/basics/variables.py
@@ -1,15 +1,13 @@
 age = 34
 name = "Adam Davidson"
 programmer = True
-print(age)
-print(name)
-
-print(f'My name: {name} and I am {age}!')
-
-print(name)
-
 career = "Machine Learning Engineer!"
 is_ML = False
+
+
+print(age)
+print(name)
+print(f'My name: {name} and I am {age}!')
 
 up_name = name.upper()
 count_name = name.count("a")
